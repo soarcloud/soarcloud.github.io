8772b2c1bde73864d24093f1dc046e7f0fd244fc
yunyouxiaoqiang <liuxiang@gmail.com>
2022-05-02 20:02:51 +0800

    change dir

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "将域名从namesilo转移到cloudflare",
+        "excerpt":"1. Login namsilo 2. Unlock domain 3. Update dns to jason.ns.cloudflare.com tricia.ns.cloudflare.com 4. Get Authorization code 5. Add payment info if not in cloudflare 6. Add site to cloudflare 7. Waiting domain available under transfer domains 8. Select domain to tranfer and finish payment 9. Approve transfer in namesilo 10....","categories": ["Blog"],
+        "tags": ["domain","hosting"],
+        "url": "/blog/migrate-domian-from-namesilo-to-cloudflare/",
+        "teaser": null
+      }]
