dd9b9f2b0acf2073a0c8a55d9595a9cefd1c45f7
yunyouxiaoqiang <liuxiang@gmail.com>
2022-05-02 20:07:10 +0800

    add new post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "使用Github Pages托管静态网站",
+        "excerpt":"使用Jekyll GitHub VSCode组合生成静态网站存储在Github Pages上。  ","categories": ["Blog"],
+        "tags": ["domain","hosting"],
+        "url": "/blog/host-blog-on-github/",
+        "teaser": null
+      },{
         "title": "将域名从namesilo转移到cloudflare",
         "excerpt":"1. Login namsilo 2. Unlock domain 3. Update dns to jason.ns.cloudflare.com tricia.ns.cloudflare.com 4. Get Authorization code 5. Add payment info if not in cloudflare 6. Add site to cloudflare 7. Waiting domain available under transfer domains 8. Select domain to tranfer and finish payment 9. Approve transfer in namesilo 10....","categories": ["Blog"],
         "tags": ["domain","hosting"],
